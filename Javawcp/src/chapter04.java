0987956f524ad378e5aa7b351e42948dd8cef6e5
花本敬太 <hanamotokeita@hanamottanoiMac.sensyu.ne.jp>
2021-02-15 17:21:47 +0900

    java2回目

---
+++ Javawcp/src/chapter04.java
@@ -0,0 +1,15 @@
+
+public class chapter04 {
+	public static void main(String[] args) {
+		String name;
+		name = "Keita Hanamoto";
+		System.out.println(name);
+		name = "今一度日本を洗濯いたし申候";
+		System.out.println(name);
+		
+		final int LUCKEY_NUMBER = 8;
+		System.out.println(LUCKEY_NUMBER);
+		
+	}
+
+}
